b6fa28c5dfde003a129e02af1cdd18b57f1b7fa2
robin <kashyap.robin.9277@gmail.com>
2022-05-05 02:39:30 +0530

    change server url

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./App";
 import axios from "axios";
 import { BrowserRouter } from "react-router-dom";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://yummybay.herokuapp.com";
 
 ReactDOM.render(
   <BrowserRouter>
